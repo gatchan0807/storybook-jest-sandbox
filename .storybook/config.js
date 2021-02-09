20c30bc83faf33a4885487afe875247a479354fc
がっちゃん <yuki.dolphin.koga@gmail.com>
2021-02-10 01:25:56 +0900

    🔧 Storybookに特定ディレクトリ配下のファイルをすべて読み込むように設定変更

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,8 @@
 import { configure } from "@storybook/vue";
 
 const loadStories = () => {
-  require("../src/stories/index");
-};
+  const req = require.context("../src/stories", true, /\.js$/);
+  req.keys().forEach(req);
+}
 
 configure(loadStories, module);
