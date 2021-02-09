5189913283ed9e1bb15c435483f655f7b61be02b
がっちゃん <yuki.dolphin.koga@gmail.com>
2021-02-10 01:02:29 +0900

    ✨ Sizeを変更できるh1タグのコンポーネントを追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <MainTitle>{{ msg }}</MainTitle>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -108,16 +108,18 @@
 <script lang="ts">
 import Vue from "vue";
 
-import HelloButton from "./HelloButton.vue"
+import HelloButton from "./HelloButton.vue";
+import MainTitle from "./MainTitle.vue";
 
 export default Vue.extend({
   name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   },
   components: {
-    HelloButton
-  }
+    HelloButton,
+    MainTitle,
+  },
 });
 </script>
 
---
+++ src/components/MainTitle.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1 :class="'title-' + size">
+    <slot />
+  </h1>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      default: "normal",
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.title-small {
+  font-size: 1rem;
+}
+
+.title-normal {
+  font-size: 2rem;
+}
+
+.title-big {
+  font-size: 4rem;
+}
+</style>
