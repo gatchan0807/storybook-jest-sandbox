182dfc4e284ab0367aa04932ae7f7ad2ef09a2b9
がっちゃん <yuki.dolphin.koga@gmail.com>
2021-02-10 10:06:30 +0900

    📸 画像スナップショットテストを行うテストの追加

---
+++ src/stories/__tests__/image.test.ts
@@ -0,0 +1,17 @@
+import initStoryshots from "@storybook/addon-storyshots";
+import { imageSnapshot } from "@storybook/addon-storyshots-puppeteer";
+import path from "path";
+
+initStoryshots({
+  suite: "Image storyshots",
+  test: imageSnapshot({
+    storybookUrl: `file://${path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "storybook-static"
+    )}`,
+  }),
+  configPath: path.resolve(__dirname, "..", "..", "..", ".storybook"),
+});
